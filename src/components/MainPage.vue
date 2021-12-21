79930acd93d45d65580288570a0144cd5f547e82
ryotaro_ubuntu <ryotaro.t.0727@gmail.com>
2021-12-21 20:39:31 +0900

    vue.jsデフォルトの表示を置き換え、ナビゲーションバーとメイン領域を表示

---
+++ src/components/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="main-page">
+        メインページです
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.min-page {
+    padding: 10px;
+}
+</style>
