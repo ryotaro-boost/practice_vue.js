5091cc1cd0b19219c30295370bc031f22d42b71f
ryotaro_ubuntu <ryotaro.t.0727@gmail.com>
2021-12-22 18:52:55 +0900

    クリックイベントによってテキストにクリック回数を表示する処理を従来のJavaScriptで記述

--- src/components/study/EventPage.vue
+++ src/components/study/EventPage.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="event-page">
     <h2> 学習3：イベント</h2>
+
+    <!-- 従来のJavaScript -->
+    <h3><span class="badge badge-secondary">JavaScript</span></h3>
+    <span id="countText">クリックされた回数は0回です</span>
+    <button id="button" class="btn btn-sm btn-info">Click Me</button>
+    <br>
+    <br>
   </div>
 </template>
 
 <script>
+// 従来のJavaScript
+let count = 0;
+window.onload = function() {
+  const button = document.getElementById('button');
+  button.onclick = function() {
+    count++;
+    const countText = document.getElementById('countText');
+    countText.innerHTML = 'クリックされた回数は' +  count + '回です ';
+  }
+}
+
 export default {
   
 }
