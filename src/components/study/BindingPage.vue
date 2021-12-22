ec39c5c7750dc053f5e2033bee799f4b29084452
ryotaro_ubuntu <ryotaro.t.0727@gmail.com>
2021-12-22 17:51:51 +0900

    従来のJavaScriptでinputに入力された文字列をリアルタイムでhtmlに出力する処理を記述

--- src/components/study/BindingPage.vue
+++ src/components/study/BindingPage.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="binding-page">
     <h2>学習1：双方向バインディング</h2>
+
+    <!-- 従来のJavaScript -->
+    <h3><span>JavaScript</span></h3>
+    <input class="form-control" id="name">
+    <span id="text">入力された名前はです</span>
+    <br>
+    <br>
   </div>
 </template>
 
 <script>
+// 従来のJavaScript
+window.onload = function() {
+  const nameInput = document.getElementById('name');
+  nameInput.oninput = function() {
+    const name = nameInput.value;
+    const text = document.getElementById('text');
+    text.innerHTML = '入力された名前は' + name + 'です';
+  }
+}
+
 export default {
   
 }
