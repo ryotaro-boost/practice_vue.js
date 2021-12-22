2a0e72fdf36bd86e6624e3479034ea0608ffbd22
ryotaro_ubuntu <ryotaro.t.0727@gmail.com>
2021-12-22 17:27:45 +0900

    学習用ページのルーティングを設置

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Notion</a>
+        <a class="navbar-brand" href="/main">Notion</a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -9,15 +9,15 @@
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fas fa-file-alt"></i>ノートリスト</a>
+                    <a class="nav-link" href="/main"><i class="fas fa-file-alt"></i>ノートリスト</a>
                 </li>
 
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown"><i class="fas fa-user-graduate"></i>学習用ページ</a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">学習1</a></li>
-                        <li><a class="dropdown-item" href="#">学習2</a></li>
-                        <li><a class="dropdown-item" href="#">学習3</a></li>
+                        <li><a class="dropdown-item" href="/study/binding">学習1</a></li>
+                        <li><a class="dropdown-item" href="/study/directive">学習2</a></li>
+                        <li><a class="dropdown-item" href="/study/event">学習3</a></li>
                     </ul>
                 </li>
             </ul>
