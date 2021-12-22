5260d574ebf51010ef4f4bcf70d16a345ec708eb
ryotaro_ubuntu <ryotaro.t.0727@gmail.com>
2021-12-22 19:01:34 +0900

    同様の処理をVue.jsで記述

--- src/components/study/EventPage.vue
+++ src/components/study/EventPage.vue
@@ -8,6 +8,11 @@
     <button id="button" class="btn btn-sm btn-info">Click Me</button>
     <br>
     <br>
+
+    <!-- Vue.js -->
+    <h3><span class="badge badge-secondary">Vue.js</span></h3>
+    <span>クリックされた回数は{{ count }}回です</span>
+    <button id="button" class="btn btn-sm btn-info" @click="onClickButton()">Click Me</button>
   </div>
 </template>
 
@@ -24,7 +29,16 @@ window.onload = function() {
 }
 
 export default {
-  
+  data() {
+    return {
+      count : 0
+    }
+  },
+  methods : {
+    onClickButton : function() {
+      this.count++;
+    }
+  }
 }
 </script>
 
