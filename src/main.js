6da166f66e0a361f2e0ecf4229e672bfa7cbb733
ryotaro_ubuntu <ryotaro.t.0727@gmail.com>
2021-12-22 14:55:50 +0900

    ルーティング下準備

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// ViewRouterを使用
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+
+  ]
+})
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
