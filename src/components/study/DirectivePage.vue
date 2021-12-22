9fdabb5e92b477dc3b957975e51523b5279bc4eb
ryotaro_ubuntu <ryotaro.t.0727@gmail.com>
2021-12-22 18:32:56 +0900

    チェックボックスのon・offによってテキストを表示する処理を従来のJavaScriptで記述

--- src/components/study/DirectivePage.vue
+++ src/components/study/DirectivePage.vue
@@ -1,10 +1,31 @@
 <template>
   <div class="directive-page">
     <h2> 学習2：ディレクティブ</h2>
+
+    <!-- 従来のJavaScript -->
+    <h3><span class="badge badge-secondary">JavaScript</span></h3>
+    <input id="check" type="checkbox" checked="checked">
+    <span id="text">チェック時のみ表示</span>
+    <br>
+    <br>
   </div>
 </template>
 
 <script>
+// 従来のJavaScript
+window.onload = function() {
+  const checkbox = document.getElementById('check');
+  checkbox.onchange = function(event) {
+    const checked = event.target.checked;
+    const text = document.getElementById('text');
+    if (checked) {
+      text.style.visibility = 'visible';
+    } else {
+      text.style.visibility = 'hidden';
+    }
+  };
+}
+
 export default {
   
 }
