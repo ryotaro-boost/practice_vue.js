bb33a8e4f94e1296d5ca1b2ec0f6743534a48f4e
ryotaro_ubuntu <ryotaro.t.0727@gmail.com>
2021-12-22 17:57:47 +0900

    同様の処理をVue.jsで記述

--- src/components/study/BindingPage.vue
+++ src/components/study/BindingPage.vue
@@ -8,6 +8,11 @@
     <span id="text">入力された名前はです</span>
     <br>
     <br>
+
+    <!-- Vue.js -->
+    <h3><span class="badge badge-secondary">Vue.js</span></h3>
+    <input class="form-control" v-model="name">
+    <span>入力された名前は{{ name }}です</span>
   </div>
 </template>
 
@@ -23,6 +28,11 @@ window.onload = function() {
 }
 
 export default {
+  data() {
+    return {
+      name: ''
+    }
+  }
   
 }
 </script>
