42cabeb073169ec6b6768f7896b08bbe0aea3ddc
ryotaro_ubuntu <ryotaro.t.0727@gmail.com>
2021-12-26 23:36:57 +0900

    v-showを使ってマウスオーバー時に各アイコンを表示

--- src/components/parts/NoteItem.vue
+++ src/components/parts/NoteItem.vue
@@ -8,6 +8,21 @@
       <i class="fas fa-file-alt"></i>
     </div>
     <div class="note-name">{{ note.name }}</div>
+
+    <div v-show="note.mouseover" class="buttons">
+      <div class="button-icon">
+        <i class="fas fa-sitemap"></i>
+      </div>
+      <div class="button-icon">
+        <i class="fas fa-plus-circle"></i>
+      </div>
+      <div class="button-icon">
+        <i class="fas fa-edit"></i>
+      </div>
+      <div class="button-icon">
+        <i class="fas fa-trash"></i>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -45,6 +60,15 @@ export default {
   .note-name {
     width: 100%;
     padding: 3px 10px;
+  }
+  .buttons {
+    display: flex;
+    flex-direction: row;
+    .button-icon {
+      padding: 3px;
+      margin-left: 3px;
+      border-radius: 5px;
+    }
   }
 }
 </style>
