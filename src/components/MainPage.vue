8591d1580e64f9139a73379889ddb8b726aa9b1a
ryotaro_ubuntu <ryotaro.t.0727@gmail.com>
2021-12-26 22:45:20 +0900

    ノート追加をhtmlに反映

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="main-page">
         <div class="left-menu">
+            <!-- ノートリスト -->
+            <div class="note" v-for="note in notelist" v-bind:key="note.id">
+                <div class="note-icon">
+                    <i class="fas fa-file-alt"></i>
+                </div>
+                <div class="note-name">{{ note.name }}</div>
+            </div>
             <!-- ノート追加ボタン -->
             <button class="transparent" @click="onClickButtonAdd">
                 <i class="fas fa-plus-square"></i>ノートを追加
@@ -37,6 +44,20 @@ export default {
     .left-menu {
         width: 350px;
         background-color: #f7f6f3;
+        .note {
+            margin: 10px 0;
+            display: flex;
+            align-items: center;
+            padding: 5px;
+            color: rgba(25, 23, 17, 0.6);
+            .note-icon {
+                margin-left: 10px;
+            }
+            .note-name {
+                width: 100%;
+                padding: 3px 10px;
+            }
+        }
     }
     .right-view {
         flex-grow: 1;
